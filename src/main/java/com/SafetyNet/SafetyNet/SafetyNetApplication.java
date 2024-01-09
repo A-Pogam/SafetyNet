75139afea9a1260e4f7bcd775043497134be76f0
A-Pogam <alicepogam59@hotmail.com>
2024-01-09 17:59:18 +0100

    added dependecy EnableJpaRepositories

--- src/main/java/com/SafetyNet/SafetyNet/SafetyNetApplication.java
+++ src/main/java/com/SafetyNet/SafetyNet/SafetyNetApplication.java
@@ -2,8 +2,14 @@ package com.SafetyNet.SafetyNet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.SafetyNet.SafetyNet.model") // Location of folder model
+@EnableJpaRepositories(basePackages = "com.SafetyNet.SafetyNet.repository") // location repository
+
+
 public class SafetyNetApplication {
 
 	public static void main(String[] args) {
