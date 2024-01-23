3aa0080d4fc8e041aed4a9ca1fcc481e3b4b289e
A-Pogam <alicepogam59@hotmail.com>
2024-01-23 17:07:03 +0100

    configured person, medicalrecord, firestation for SNApplication
    + try to open url but failed

--- src/main/java/com/SafetyNet/SafetyNet/SafetyNetApplication.java
+++ src/main/java/com/SafetyNet/SafetyNet/SafetyNetApplication.java
@@ -1,5 +1,7 @@
 package com.SafetyNet.SafetyNet;
 
+import model.FireStation;
+import model.MedicalRecord;
 import service.FireStationService;
 import service.MedicalRecordService;
 import service.PersonService;
@@ -20,7 +22,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.SafetyNet.SafetyNet.model")
@@ -28,7 +32,7 @@ import java.util.List;
 @ComponentScan(basePackages = {"com.SafetyNet.SafetyNet.service"})
 public class SafetyNetApplication {
 
-	@Autowired //inject dependency
+	@Autowired
 	private PersonService personService;
 
 	@Autowired
@@ -41,11 +45,79 @@ public class SafetyNetApplication {
 		SpringApplication.run(SafetyNetApplication.class, args);
 	}
 
-	@PostConstruct //method loaddata will be called after creation instance safetynetApplication, used by Spring when app start
-	public void loadData() throws IOException { //read JSON file and convert (list, savelall, personservice)
+	@PostConstruct
+	public void loadData() throws IOException {
 		ObjectMapper objectMapper = new ObjectMapper();
 		InputStream inputStream = getClass().getResourceAsStream("/data/safety-net-data.json");
-		List<Person> persons = objectMapper.readValue(inputStream, new TypeReference<>() {});
+		Map<String, List<Map<String, Object>>> data = objectMapper.readValue(inputStream, new TypeReference<>() {
+		});
+
+		// Load MedicalRecord data
+		List<Map<String, Object>> medicalRecordsData = data.get("medicalrecords");
+		if (medicalRecordsData != null) {
+			for (Map<String, Object> medicalRecordData : medicalRecordsData) {
+				MedicalRecord medicalRecord = convertToMedicalRecord(medicalRecordData);
+				medicalRecordService.addMedicalRecord(medicalRecord);
+			}
+		}
+
+		// Load FireStation data
+		List<Map<String, Object>> fireStationsData = data.get("firestations");
+		if (fireStationsData != null) {
+			for (Map<String, Object> fireStationData : fireStationsData) {
+				FireStation fireStation = convertToFireStation(fireStationData);
+				fireStationService.addMapping(fireStation);
+			}
+		}
+
+		// Load Person data
+		List<Map<String, Object>> personsData = data.get("persons");
+		if (personsData != null) {
+			for (Map<String, Object> personData : personsData) {
+				Person person = convertToPerson(personData);
+				personService.addPerson(person);
+			}
+		}
+	}
+
+	private MedicalRecord convertToMedicalRecord(Map<String, Object> data) {
+		MedicalRecord medicalRecord = new MedicalRecord();
+		medicalRecord.setFirstName((String) data.get("firstName"));
+		medicalRecord.setLastName((String) data.get("lastName"));
+		medicalRecord.setBirthdate((String) data.get("birthdate"));
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		// using ObjectMapper to convert to List<String>
+		List<String> medications = objectMapper.convertValue(data.get("medications"), new TypeReference<>() {});
+		List<String> allergies = objectMapper.convertValue(data.get("allergies"), new TypeReference<>() {});
+
+		medicalRecord.setMedications(medications != null ? medications : new ArrayList<>());
+		medicalRecord.setAllergies(allergies != null ? allergies : new ArrayList<>());
+		return medicalRecord;
 
 	}
+
+	private FireStation convertToFireStation(Map<String, Object> data) {
+		FireStation fireStation = new FireStation();
+		fireStation.setAddress((String) data.get("address"));
+		fireStation.setStation(Integer.parseInt(String.valueOf(data.get("station"))));
+		return fireStation;
+	}
+
+	private Person convertToPerson(Map<String, Object> data) {
+		Person person = new Person();
+		person.setFirstname((String) data.get("firstName"));
+		person.setLastname((String) data.get("lastName"));
+		person.setAddress((String) data.get("address"));
+		person.setCity((String) data.get("city"));
+		person.setZip((String) data.get("zip"));
+		person.setPhone((String) data.get("phone"));
+		person.setEmail((String) data.get("email"));
+		return person;
+	}
+
+
+
+
 }
--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -11,6 +11,12 @@ public class Person {
 
     private String firstname;
     private String lastname;
+    private String address;
+    private String city;
+    private String zip;
+    private String phone;
+    private String email;
+
 
     public String getFirstname() {
         return firstname;
@@ -27,11 +33,46 @@ public class Person {
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
-    private String address;
-    private String city;
-    private String zip;
-    private String phone;
-    private String email;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     @OneToOne(mappedBy = "person") //One only entry by person, avoid to write again same date for same person
     private MedicalRecord medicalRecord;
